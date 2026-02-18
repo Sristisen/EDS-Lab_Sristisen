9b33ce6bbd9378639b4af606d55c6f4e092ea580
Sristisen <sristisen14@gmail.com>
2026-02-18 13:04:47 +0530

    Add files via upload

--- Eds assignment.py
+++ Eds assignment.py
@@ -1,3 +1,22 @@
-C=float(input("ENTER TEMP IN CELSIUS: "))
-F=(9/5)*C+32
-print(F"{F:.2f}")
+## CODE FOR SAMPLE ASSIGNMENT
+
+# C=float(input("ENTER TEMP IN CELSIUS: "))
+# F=(9/5)*C+32
+# print(F"{F:.2f}")
+
+## NUMBER GUESSING GAME (SELF PROJECT)
+
+import random
+print("......NUMEBER GUESSING GAME........")
+n=random.randint(1,100)
+attempts=0
+while True:
+    guess= int(input("ENTER A NUMBER:"))
+    attempts+=1
+    if guess<n:
+        print("TOO LOW: TRY AGAIN")
+    elif guess>n:
+        print("TOO HIGH: TRY AGAIN")
+    else:
+        print("YOU GUESSED IT")
+        print("YOU GUESSED IT IN",attempts,"ATTEMPTS")
